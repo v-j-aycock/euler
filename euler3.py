b21b4369fef19185455c41e1e1d07ea51693a5a9
v-j-aycock <vjaycock1@gmail.com>
2025-07-28 13:02:27 -0400

    Update euler3.py

--- euler3.py
+++ euler3.py
@@ -1,20 +1,18 @@
-testPrime=int(input())
-primeRange=range(2,testPrime)
-#head in the turing sense, our "home" position
-rangeHead = 0
-def generateMultiples(primeRange, rangeHead):
-    rangeIter=primeRange[rangeHead]
-    rangeRemove=[]
-    nonPrime=0
-    #timeRun starts at 1 to skip the first instance of the number
-    timeRun=1
-    try:
-        while nonPrime<testPrime:
-            nonPrime=primeRange[(rangeIter*timeRun)]
-            rangeRemove.append(nonPrime)
-            timeRun+=1
-    except: 
-        primeRange=[x for x in primeRange if x not in rangeRemove]
-        rangeHead+=1
-        return primeRange
-print(generateMultiples(primeRange,rangeHead))
+needFactors=int(input())
+primeRange=list(range(2,needFactors+1))
+composites=set(())
+for x in primeRange:
+    y=2
+    while x*y < needFactors:
+        composites.add(x*y)
+        y+=1
+        if primeRange[-1] % x:
+            composites.add(primeRange[-1])
+primeRange=[x for x in primeRange if x not in composites]
+x=-1
+while x>-len(primeRange):
+    if needFactors % primeRange[x]==0:
+        print(primeRange[x])
+        break
+    else:
+        x-=1
