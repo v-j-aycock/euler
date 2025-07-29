44291efedb3260625287db9f94fb0c7239834a10
v-j-aycock <vjaycock1@gmail.com>
2025-07-29 10:09:43 -0400

    Update euler3.py

--- euler3.py
+++ euler3.py
@@ -1,18 +1,15 @@
+#get target number, set x to smallest prime
 needFactors=int(input())
-primeRange=list(range(2,needFactors+1))
-composites=set(())
-for x in primeRange:
-    y=2
-    while x*y < needFactors:
-        composites.add(x*y)
-        y+=1
-        if primeRange[-1] % x:
-            composites.add(primeRange[-1])
-primeRange=[x for x in primeRange if x not in composites]
-x=-1
-while x>-len(primeRange):
-    if needFactors % primeRange[x]==0:
-        print(primeRange[x])
+x=2
+#true loop, breaks when done
+while True:
+    #checks if needFactors can be evenly divided by x, does it if so. continues
+    if needFactors%x == 0:
+        needFactors = needFactors/x
+        continue
+    #checks if needFactors/x is the same. could also be needFactors == x. prints and ends program if so
+    if int(needFactors/x) == 1:
+        print(int(needFactors))
         break
-    else:
-        x-=1
+    #adds 1 to x to iterate through numbers for primes
+    x+=1
