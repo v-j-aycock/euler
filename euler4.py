e17c35ab7aa38c8add136b8a9069d1870cad48f5
Vince <vjaycock1@gmail.comgit config --global user.email vjaycock1@gmail.comgit config --global user.email vjaycock1@gmail.com>
2025-07-29 11:54:10 -0400

    new file:   euler4.py

---
+++ euler4.py
@@ -0,0 +1,28 @@
+import sys
+x=999
+y=999
+
+while x>99 and y>99:
+    #trueList contains a 1 if str[z] == str[-z] and. will be checked to see if it is the same length as productStr to confirm palindrome. a 0 should break the loop
+    trueList=[]
+    z=0
+    productStr=str(x*y)
+    print(productStr)
+    while z < int((len(productStr)-1)/2)+1:
+        if len(trueList) == len(productStr) and 0 not in trueList:
+            print(x,'*',y,"=",productStr)
+            sys.exit()
+        elif 0 in trueList:
+            z+=20
+        elif productStr[z] == productStr[-z-1]:
+            trueList.append(1)
+            z+=1
+        else:
+            trueList.append(0)
+            z+=1
+    print(trueList)
+
+    if y>=x:
+        y-=1
+    if x>y:
+        x-=1
