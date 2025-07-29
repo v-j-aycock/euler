b3f1b1e676cd275ade73843ccfc346cb46dd9e37
Vince <vjaycock1@gmail.comgit config --global user.email vjaycock1@gmail.comgit config --global user.email vjaycock1@gmail.com>
2025-07-29 13:17:48 -0400

    modified:   euler4.py

--- euler4.py
+++ euler4.py
@@ -1,13 +1,12 @@
 import sys
 x=999
 y=999
-
+previousQuotients=[]
 while x>99 and y>99:
     #trueList contains a 1 if str[z] == str[-z] and. will be checked to see if it is the same length as productStr to confirm palindrome. a 0 should break the loop
     trueList=[]
     z=0
     productStr=str(x*y)
-    print(productStr)
     while z < int((len(productStr)-1)/2)+1:
         if 0 in trueList:
             z+=20
@@ -15,13 +14,15 @@ while x>99 and y>99:
             trueList.append(1)
             z+=1
             if len(trueList) == int(len(str(productStr))/2):
-                print(x,'*',y,'=',x*y)
-                sys.exit()
+                previousQuotients.append(x*y)
+                print(x,"*",y,'=',x*y)
         else:
             trueList.append(0)
             z+=1
-    print(trueList)
-    if y>=x:
-        y-=1
-    if x>y:
+    if x==y:
         x-=1
+        y=999
+    if x!=y:
+        y-=1
+previousQuotients.sort()
+print(previousQuotients[-1])
