923cf1989f65c596d0d286a71c66b6cda1997c53
Vince <vjaycock1@gmail.comgit config --global user.email vjaycock1@gmail.comgit config --global user.email vjaycock1@gmail.com>
2025-08-01 10:42:19 -0400

    new file, potential module:   prime.py

---
+++ prime.py
@@ -0,0 +1,9 @@
+def prime(targetNum):
+    import math
+    sqrNum = int(math.sqrt(targetNum))
+    while sqrNum > 1:
+        if targetNum % sqrNum == 0:
+            return 0
+        sqrNum-=1
+    return 1
+print(prime(int(input())))
